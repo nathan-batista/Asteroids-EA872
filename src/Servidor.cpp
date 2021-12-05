3e89db4f56f6a82cf1b476e1f286c27abebf9b1f
AriadneBigheti <ariadne.bigheti@hotmail.com>
2021-12-05 17:08:08 -0300

    criei model e controller no servidor

--- src/Servidor.cpp
+++ src/Servidor.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include "json.hpp"
 #include <fstream>
+#include <boost/asio.hpp>
 
 using namespace std;
 using nlohmann::json;
@@ -15,6 +16,7 @@ int main(){
     char v[10000];
     char resp;
     json j;
+    vector<Nave> listaDeNaves;
 
     boost::asio::io_service my_io_service; // Conecta com o SO
 
@@ -40,9 +42,22 @@ int main(){
         std::cin >> resp;
     }
 
+    // Enviando ID pro cliente
     int num_players = listaDeClientes.size();
+
+    for(int i=0; i<num_players; i++){
+        std::string msg(str(i));
+        my_socket.send_to(boost::asio::buffer(msg), listaDeClientes[i]);
+    }
+  
     for(int i = 0; i<num_players;i++){
         vector<Tiro> tiros;
-        Nave nave = Nave(1, 1, 0, 320, 120, 30, 0.1, tiros);
+        Nave nave = Nave(1, 1, 0, 320, 120, 30, 0.1, tiros, i);
+        listaDeNaves.push_back(nave);
     }
+
+    ModelFinal model = ModelFinal(listaDeNaves);
+    ControllerGeral controller = ControllerGeral(model);
+
+    return 0;
 }
